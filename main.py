f5cd6b5300eeb68d231bc5bae0b7f098c204ecdf
cedric3347 <ccterry04@gmail.com>
2025-06-17 22:21:20 -0400

    added notes, instructions, and sys.argv for command-line prompts

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
-import os 
+import os
+import sys 
 from dotenv import load_dotenv
 from  google import genai
 
+def main():
+    
+    # sends prompt to Gemini API
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+    # user prompt conversion
+    prompt = ' '.join(sys.argv[1:])
+    
+    #check for sys.argv argument
+    if len(sys.argv) > 1 :
+        response = client.models.generate_content(model='gemini-2.0-flash-001', contents=prompt)
 
-client = genai.Client(api_key=api_key)
+    # gives insturctions on how to use program   
+    else:
+        print('This program requires a command-line argument')
+        print('Usage: python3 main.py <enter your prompt here>')
+        print("No prompt detected...Exiting Program")
+        sys.exit(1)
 
+    # prints outputs
+    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    print(f'Response:\n {response.text}')
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
-
-
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if __name__== "__main__":
+    main()
