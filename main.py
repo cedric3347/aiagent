eec74b22a9dc5a53e2548da627b2cf1b6b5ccc0a
cedric3347 <ccterry04@gmail.com>
2025-06-18 00:26:16 -0400

    added --verbose flag functionality and print changes

--- main.py
+++ main.py
@@ -17,20 +17,29 @@ def main():
 
     messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
     
-    #check for sys.argv argument
+    # check for sys.argv argument
     if len(sys.argv) > 1 :
         response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
 
-    # gives insturctions on how to use program   
+
+        # checks for "--verbose" flag and prints more info about a prompt
+        if sys.argv[-1] == "--verbose":
+            split_prompt = prompt.split(' ')
+            prompt_without_last = split_prompt[:-1]
+            print(f'User prompt: "{' '.join(prompt_without_last)}"')
+            print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+            print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+
+
+    # gives insturctions on how to use the program   
     else:
-        print('This program requires a command-line argument')
-        print('Usage: python3 main.py <enter your prompt here>')
+        print('This program requires a command-line argument...\n')
+        print('Usage: \n python3 main.py <enter your prompt here> \n')
+        print('For more information use the flag "--verbose" at the end of your prompt\n')
         print("No prompt detected...Exiting Program")
         sys.exit(1)
 
     # prints outputs
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
     print(f'Response:\n {response.text}')
 
 if __name__== "__main__":
